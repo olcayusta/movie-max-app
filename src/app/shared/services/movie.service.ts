b7ac05a21441c14e10ab452c9daf150678fb843b
olcayusta <olcayusta02@gmail.com>
2021-01-26 07:40:35 +0300

    ng update

---
+++ src/app/shared/services/movie.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  constructor(private http: HttpClient) {}
+
+  getMovie(): void {}
+}
